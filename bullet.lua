bdfae9fcfbc4e693755dcbfa4dc3657baf4544c7
Matthew Rathkey <rathkeym@gmail.com>
2023-02-24 19:06:41 -0700

    Updated to allow shooting in multiple directions
    Player can now shoot up/down/left/right but I think the code can be
    cleaned up considerably. Right now the multiple directions are
    implemented through a web of if statements. I need to learn more about
    tables/classes in Lua in order to improve this.

--- bullet.lua
+++ bullet.lua
@@ -1,24 +1,99 @@
 Bullet = Class{}
 
-local bullet_speed = 300
+bullet_speed = 300
 
-function Bullet:init(xpos, ypos)
+local direction_left = "left"
+local direction_right = "right"
+local direction_up = "up"
+local direction_down = "down"
+
+function Bullet:init(player, direction)
     self.texture = love.graphics.newImage("bullet.png")
     self.width = self.texture:getWidth()
     self.height = self.texture:getHeight()
-    self.x = xpos
-    self.y = ypos
-    self.dy = bullet_speed
+    self.direction = direction
+    self.orientation = 0
+    -- set x and y position according to turret's direction, bearing rotation in mind
+    if self.direction == direction_left then
+        self.x = player.x
+        self.y = player.y + player.height/2 - self.width/2
+        self.orientation = math.pi/2
+    elseif self.direction == direction_right then
+        self.x = player.x + player.width + self.height
+        self.y = player.y + player.height/2 - self.width/2
+        self.orientation = math.pi/2
+    elseif self.direction == direction_up then
+        self.x = player.x + player.width/2 - self.width/2
+        self.y = player.y - self.height
+    else -- assumes bullet must be down
+        self.x = player.x + player.width/2 - self.width/2
+        self.y = player.y + player.height
+    end
+    self.speed = bullet_speed
     self.out_of_play = false
+
+    -- Don't understand tables/classes well enough yet to make this work
+    --[[ store update functions in a table
+    self.move = {}
+    self.move[direction_left] = Bullet:update_left(dt)
+    self.move[direction_right] = Bullet:update_right(dt)
+    self.move[direction_up] = Bullet:update_up(dt)
+    self.move[direction_down] = Bullet:update_down(dt) ]]
 end
 
 function Bullet:update(dt)
-    self.y = self.y + self.dy*dt
-    if self.y>WINDOW_HEIGHT then -- bullet is offscreen
+    if self.direction == direction_left then
+        self.x = self.x - self.speed*dt
+        if self.x<0 then -- bullet is offscreen
         self.out_of_play = true
+        end
+    elseif self.direction == direction_right then
+        self.x = self.x + self.speed*dt
+        if self.x>(WINDOW_WIDTH+self.height) then -- bullet is offscreen
+            self.out_of_play = true
+        end
+    elseif self.direction == direction_up then
+        self.y = self.y - self.speed*dt
+        if self.y<(0-self.height) then -- bullet is offscreen
+            self.out_of_play = true
+        end
+    else -- assumes bullet must be down
+        self.y = self.y + self.speed*dt
+        if self.y>WINDOW_HEIGHT then -- bullet is offscreen
+            self.out_of_play = true
+        end
     end
 end
 
 function Bullet:draw()
-    love.graphics.draw(self.texture, self.x, self.y)
+    love.graphics.draw(self.texture, self.x, self.y, self.orientation)
 end
+
+--[[
+function Bullet:update_left(dt)
+    self.x = self.x - self.speed*dt
+    if self.x<(0-self.width) then -- bullet is offscreen
+        self.out_of_play = true
+    end
+end
+
+function Bullet:update_right(dt)
+    self.x = self.x + self.speed*dt
+    if self.x>WINDOW_WIDTH then -- bullet is offscreen
+        self.out_of_play = true
+    end
+end
+
+function Bullet:update_up(dt)
+    self.y = self.y - self.speed*dt
+    if self.y<(0-self.height) then -- bullet is offscreen
+        self.out_of_play = true
+    end
+end
+
+function Bullet:update_down(dt)
+    self.y = self.y + self.speed*dt
+    if self.y>WINDOW_HEIGHT then -- bullet is offscreen
+        self.out_of_play = true
+    end
+end ]]
--- player.lua
+++ player.lua
@@ -5,7 +5,10 @@ Player = Class{}
 local player_xpos = 100
 local player_ypos = 10
 local player_speed = 180
+
+--[[ scrapped this
 local player_max_ypos = 140 -- max distance player can move down on screen
+]]
 
 local direction_left = "left"
 local direction_right = "right"
@@ -38,33 +41,19 @@ function Player:init()
 end
 
 function Player:update(dt)
+    -- Press arrow keys to move
     if love.keyboard.isDown('left') then
         self.dx = -player_speed
     elseif love.keyboard.isDown('right') then
         self.dx = player_speed
     else self.dx = 0
     end
-
     if love.keyboard.isDown('up') then
         self.dy = -player_speed
     elseif love.keyboard.isDown('down') then
         self.dy = player_speed
     else
         self.dy = 0
-    end
-
-    -- Press 'space' to shoot
-    if love.keyboard.pressed['space'] then
-        if ammo_remaining > 0 then
-            bullet = Bullet(self.x + (self.width/2), self.y + self.height)
-            table.insert(all_bullets, bullet)
-            ammo_remaining = ammo_remaining - 1
-        end
-    end
-
-    -- Press 'r' to reload
-    if love.keyboard.pressed['r'] then
-        ammo_remaining = max_ammo
     end
 
     -- Press 'wasd' to change turret direction
@@ -79,6 +68,20 @@ function Player:update(dt)
         self.direction = direction_right
     end
 
+    -- Press 'space' to shoot
+    if love.keyboard.pressed['space'] then
+        if ammo_remaining > 0 then
+            bullet = Bullet(self, self.direction)
+            table.insert(all_bullets, bullet)
+            ammo_remaining = ammo_remaining - 1
+        end
+    end
+
+    -- Press 'r' to reload
+    if love.keyboard.pressed['r'] then
+        ammo_remaining = max_ammo
+    end
+
     -- update player
     self.x = self.x + self.dx*dt
     self.y = self.y + self.dy*dt
@@ -86,7 +89,7 @@ function Player:update(dt)
     if self.x<0 then self.x=0
     elseif self.x>(WINDOW_WIDTH-self.width) then self.x=(WINDOW_WIDTH-self.width) end
     if self.y<0 then self.y=0
-    elseif self.y>(player_max_ypos-self.height) then self.y=(player_max_ypos-self.height) end
+    elseif self.y>(WINDOW_HEIGHT-self.height) then self.y=(WINDOW_HEIGHT-self.height) end
 
     -- update bullet(s)
     for _, v in ipairs(all_bullets) do
