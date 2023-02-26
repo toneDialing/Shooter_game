3c97129cc94f1b1798afef08c0d3b2f56e1e0933
Matthew Rathkey <rathkeym@gmail.com>
2023-02-26 16:23:37 -0700

    Moved all bullet update functions into a table
    The table self.move now contains anonymous functions to update a
    bullet's position based upon its direction. This eliminates the need for
    if statements in Bullet:update(dt).

--- bullet.lua
+++ bullet.lua
@@ -32,71 +32,43 @@ function Bullet:init(player, direction)
     self.speed = bullet_speed
     self.out_of_play = false
 
-    -- Don't understand tables/classes well enough yet to make this work
-    --[[ store update functions in a table
-    self.move = {}
-    self.move[direction_left] = Bullet:update_left(dt)
-    self.move[direction_right] = Bullet:update_right(dt)
-    self.move[direction_up] = Bullet:update_up(dt)
-    self.move[direction_down] = Bullet:update_down(dt) 
-    
-    Write functions here directly instead?
+    --[[
+        self.move:
+        Table containing anonymous functions that are called depending
+        upon the bullet's direction.
     ]]
+    self.move = {
+        [direction_left] = function(dt)
+            self.x = self.x - self.speed*dt
+            if self.x<0 then -- bullet is offscreen
+                self.out_of_play = true
+            end
+        end,
+        [direction_right] = function(dt)
+            self.x = self.x + self.speed*dt
+            if self.x>(WINDOW_WIDTH+self.height) then -- bullet is offscreen
+                self.out_of_play = true
+            end
+        end,
+        [direction_up] = function(dt)
+            self.y = self.y - self.speed*dt
+            if self.y<(0-self.height) then -- bullet is offscreen
+                self.out_of_play = true
+            end
+        end,
+        [direction_down] = function(dt)
+            self.y = self.y + self.speed*dt
+            if self.y>WINDOW_HEIGHT then -- bullet is offscreen
+                self.out_of_play = true
+            end
+        end
+    }
 end
 
 function Bullet:update(dt)
-    if self.direction == direction_left then
-        self.x = self.x - self.speed*dt
-        if self.x<0 then -- bullet is offscreen
-            self.out_of_play = true
-        end
-    elseif self.direction == direction_right then
-        self.x = self.x + self.speed*dt
-        if self.x>(WINDOW_WIDTH+self.height) then -- bullet is offscreen
-            self.out_of_play = true
-        end
-    elseif self.direction == direction_up then
-        self.y = self.y - self.speed*dt
-        if self.y<(0-self.height) then -- bullet is offscreen
-            self.out_of_play = true
-        end
-    else -- assumes bullet must be down
-        self.y = self.y + self.speed*dt
-        if self.y>WINDOW_HEIGHT then -- bullet is offscreen
-            self.out_of_play = true
-        end
-    end
+    self.move[self.direction](dt)
 end
 
 function Bullet:draw()
     love.graphics.draw(self.texture, self.x, self.y, self.orientation)
 end
-
---[[
-function Bullet:update_left(dt)
-    self.x = self.x - self.speed*dt
-    if self.x<(0-self.width) then -- bullet is offscreen
-        self.out_of_play = true
-    end
-end
-
-function Bullet:update_right(dt)
-    self.x = self.x + self.speed*dt
-    if self.x>WINDOW_WIDTH then -- bullet is offscreen
-        self.out_of_play = true
-    end
-end
-
-function Bullet:update_up(dt)
-    self.y = self.y - self.speed*dt
-    if self.y<(0-self.height) then -- bullet is offscreen
-        self.out_of_play = true
-    end
-end
-
-function Bullet:update_down(dt)
-    self.y = self.y + self.speed*dt
-    if self.y>WINDOW_HEIGHT then -- bullet is offscreen
-        self.out_of_play = true
-    end
-end ]]
