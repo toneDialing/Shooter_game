984dbc77197241e49e0ca42adab2186e7092cc1c
Matthew Rathkey <rathkeym@gmail.com>
2023-03-05 01:36:41 -0700

    Simplified collision function

--- collision.lua
+++ collision.lua
@@ -2,17 +2,13 @@
     width and height, so this function works sensibly for either. 
 ]]
 function bullet_collision(bullet, object)
-    if (bullet.x + bullet.width) > object.x and bullet.x < (object.x + object.width) and
-        (bullet.y + bullet.height) > object.y and bullet.y < (object.y + object.height) then
-        return true
-    end
+    return (bullet.x + bullet.width) > object.x and bullet.x < (object.x + object.width) and
+        (bullet.y + bullet.height) > object.y and bullet.y < (object.y + object.height)
 end
 
 --[[ More generalized version:
 function collision(subject, object)
-    if (subject.x + subject.width) > object.x and subject.x < (object.x + object.width) and
-        (subject.y + subject.height) > object.y and subject.y < (object.y + object.height) then
-            return true
-    end
+    return (subject.x + subject.width) > object.x and subject.x < (object.x + object.width) and
+        (subject.y + subject.height) > object.y and subject.y < (object.y + object.height)
 end
 ]]
