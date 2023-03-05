ca35059255d72340a5241e7d7951206c2340998b
Matthew Rathkey <rathkeym@gmail.com>
2023-03-04 17:55:49 -0700

    Change color of diagonal enemies

--- enemy3.lua
+++ enemy3.lua
@@ -6,6 +6,8 @@ Diag_enemy = Class{__includes = Enemy}
 
 function Diag_enemy:init(x_pos, y_pos, speed)
     Enemy.init(self, x_pos, y_pos)
+    self.texture = love.graphics.newImage("graphics/enemy_orange.png")
+    -- Note width and height are still accurate as all enemies are same size for now
     self.dx = speed
     self.dy = speed
 end
