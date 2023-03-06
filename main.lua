79aa9ee11e4efe9afe5f302f7d7a8ea1749d7b2e
Matthew Rathkey <rathkeym@gmail.com>
2023-03-06 00:29:40 -0700

    Added basic home screen
    I now have several game states, and I don't like how main.lua is
    starting to get cluttered. Some tidying up is still needed.

--- main.lua
+++ main.lua
@@ -25,6 +25,9 @@ function love.load()
     love.window.setTitle("Six Shots")
     love.graphics.setDefaultFilter("nearest", "nearest")
 
+    standard_font = love.graphics.newFont("fonts/Capture_it.ttf", 30)
+    love.graphics.setFont(standard_font)
+
     -- tables to keep track of which keys have been pressed once or released
     -- refreshes after each update (see end of love.update() below)
     love.keyboard.pressed = {}
@@ -32,7 +35,7 @@ function love.load()
 
     map = Map()
 
-    game_state = "play"
+    game_state = "home_screen"
 end
 
 function love.keypressed(key)
@@ -50,10 +53,14 @@ end
 function love.update(dt)
     if game_state == "play" then
         map:update(dt)
-    elseif game_state == "death" or game_state == "level_clear" then
-        if love.keyboard.pressed['p'] then
+    elseif game_state == "death" or game_state == "home_screen" then
+        if love.keyboard.pressed['p'] or love.keyboard.pressed['return'] then
             map = Map()
             game_state = "play"
+        end
+    elseif game_state == "level_clear" then
+        if love.keyboard.pressed['p'] or love.keyboard.pressed['return'] then
+            game_state = "home_screen"
         end
     end
 
@@ -63,5 +70,9 @@ function love.update(dt)
 end
 
 function love.draw()
-    map:draw()
+    if game_state == "home_screen" then
+        love.graphics.printf("66 Shots\nPress 'p' to play", 0, 100, WINDOW_WIDTH, "center")
+    else
+        map:draw()
+    end
 end
