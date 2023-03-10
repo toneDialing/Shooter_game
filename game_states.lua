8c9728035cdf02d1050979582305bbf8edf96f4d
Matthew Rathkey <rathkeym@gmail.com>
2023-03-10 00:13:55 -0700

    Updated table and added comment

--- game_states.lua
+++ game_states.lua
@@ -1,6 +1,13 @@
+-- This currently isn't working for some reason. Need to review tables in Lua again.
+
+local play = "play"
+local death = "death"
+local level_clear = "level clear"
+local home_screen = "home screen"
+
 game_states = {
-    [play] = "play",
-    [death] = "death",
-    [level_clear] = "level clear",
-    [home_screen] = "home screen"
+    [play] = 1,
+    [death] = 2,
+    [level_clear] = 3,
+    [home_screen] = 4
 }
